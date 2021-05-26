0929645e31ef503536d68e4bd03758dfda060173
Killerlord1988 <killerlord1988@gmail.com>
2021-05-26 23:20:20 +0400

    add validator phone

---
+++ build/js/anchor.js
@@ -0,0 +1,18 @@
+var links = document.querySelectorAll('[data-link]');
+
+if (links) {
+  for (var i = 0; i < links.length; i++) {
+
+    links[i].addEventListener('click', function (evt) {
+
+      var target = document.querySelector('[id=' + evt.target.dataset.link + ']');
+
+      if (target) {
+        target.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    });
+  }
+};
