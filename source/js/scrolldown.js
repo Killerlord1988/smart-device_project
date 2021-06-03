b55edd6234e56d7a98caa2d45719ac7c753a119e
Killerlord1988 <killerlord1988@gmail.com>
2021-06-03 15:18:14 +0400

    Text and content blocks are styled without class

--- source/js/scrolldown.js
+++ source/js/scrolldown.js
@@ -1,6 +1,5 @@
 'use strict';
-
-var promoScroll = document.querySelector('.promo__scroll');
+var promoScroll = document.querySelector('.promo__scroll p');
 
 promoScroll.addEventListener('click', function () {
   window.scrollTo(0, 1000);
