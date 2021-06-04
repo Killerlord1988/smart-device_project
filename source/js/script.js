564cc2022ef094d51469e4ede6f040f1e77a849a
Killerlord1988 <killerlord1988@gmail.com>
2021-06-04 11:44:29 +0400

    add no-js

--- source/js/script.js
+++ source/js/script.js
@@ -3,3 +3,8 @@
 $('#phone').mask('+7 (999) 999-99-99');
 $('#call-phone').mask('+7 (999) 999-99-99');
 /* eslint-enable */
+
+var noJs = document.querySelectorAll('.no-js');
+noJs.forEach(function (n) {
+  n.classList.remove('no-js');
+});
