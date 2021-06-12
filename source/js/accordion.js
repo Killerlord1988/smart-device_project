4cee6275345a3164c5c8dcfe7db7c97a865ffe81
Killerlord1988 <killerlord1988@gmail.com>
2021-06-12 18:22:30 +0400

    fix accordion

--- source/js/accordion.js
+++ source/js/accordion.js
@@ -1,23 +1,36 @@
 'use strict';
 
 var acc = document.getElementsByClassName('accordion');
+var panels = document.getElementsByClassName('footer__accordion-panel');
 
 document.querySelectorAll('.footer__content .footer__accordion-panel--no-js').forEach(function (n) {
   n.classList.remove('footer__accordion-panel--no-js');
 });
 
+function removeActiveClass(arr, cls) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].classList.contains(cls)) {
+      arr[i].classList.remove(cls);
+    }
+  }
+}
+
+function closeAllAccordion() {
+  removeActiveClass(acc, 'accordion--active');
+  removeActiveClass(panels, 'footer__accordion-panel--active');
+}
+
 for (var i = 0; i < acc.length; i++) {
   acc[i].addEventListener('click', function (evt) {
+    closeAllAccordion();
+
     evt.target.classList.toggle('accordion--active');
 
     var panel = evt.target.nextElementSibling;
     if (panel.classList.contains('footer__accordion-panel--active')) {
       panel.classList.remove('footer__accordion-panel--active');
-      panel.classList.add('footer__accordion-panel');
-
     } else {
       panel.classList.add('footer__accordion-panel--active');
-      panel.classList.remove('footer__accordion-panel');
     }
   });
 }
