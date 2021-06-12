e76ffad14ebd7616d6970fa7d58acc907246f7dc
Killerlord1988 <killerlord1988@gmail.com>
2021-06-12 13:31:05 +0400

    fix focus on scroll down

--- source/js/scrolldown.js
+++ source/js/scrolldown.js
@@ -1,6 +1,16 @@
 'use strict';
 var promoScroll = document.querySelector('.promo__scroll p');
 
+function scroll(posX, posY) {
+  window.scrollTo(posX, posY);
+}
+
 promoScroll.addEventListener('click', function () {
-  window.scrollTo(0, 1000);
+  scroll(0, 1000);
+});
+
+promoScroll.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    scroll(0, 1000);
+  }
 });
